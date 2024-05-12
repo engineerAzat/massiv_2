118a55a08b2b3374652e53dfcadec9f9180d0867
Azat <vildus@yandex.ru>
2024-05-12 13:46:21 +0300

    12.05.2024

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        // Первое задание
+        System.out.println("\nTask 1\n");
+
     }
 }
